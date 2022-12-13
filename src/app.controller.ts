8ccfac7c56487ff91c12f9e0924faeefb2906085
luiz <lguilhermefl@gmail.com>
2022-12-13 14:36:22 -0300

    feat: add post create notification

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { randomUUID } from 'crypto';
 import { PrismaService } from './prisma.service';
 
 @Controller('notifications')
@@ -8,5 +9,17 @@ export class AppController {
   @Get()
   getHello() {
     return this.prisma.notification.findMany();
+  }
+
+  @Post()
+  async create() {
+    await this.prisma.notification.create({
+      data: {
+        id: randomUUID(),
+        content: 'Você tem uma nova solicitação de amizade!',
+        category: 'social',
+        recipientId: randomUUID(),
+      },
+    });
   }
 }
